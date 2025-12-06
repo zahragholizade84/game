90ce43d4ab11059377341284abf45127307d3d0a
zahra gholizade <zhragholizade84>
2025-12-06 06:23:31 -0800

    Removing div from html and creating a zombie function in JS and adding zombies in three seconds and a function to check the collision location

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btnjump = document.querySelector(".btn");
 const mush =document.querySelector(".mush");
 const mushimg =document.querySelector(".img-mush");
-
+//پرش قارچ
 btnjump.addEventListener("click", ()=>{
     mush.classList.remove("jump");
     void mush.offsetWidth;
@@ -13,31 +13,53 @@ document.addEventListener("keydown", (e)=>{
     void mush.offsetWidth;
     mush.classList.add("jump")
 })
+//ارایه نگه دارنده زامبی 
+const zombies =[];
+const speed= 5;
+//ساخت تابع زامبی
+function createZombie() {
+    const zombie = document.createElement("div");
+    zombie.classList.add("zombia");
 
-function gameOver(){
-    clearInterval(gameLoop);
-    alert("done");
+    const img = document.createElement("img");
+    img.src = "walk/go_1.png";
+    img.classList.add("zum");
+
+    zombie.appendChild(img);
+    document.body.appendChild(zombie);
+
+    return {
+        el: zombie,
+        img: img,
+        frame: 1,
+        pos: -150
+    };
 }
+//اضافه شذن زامبی هر چند ثانیه
 
-
-let frame = 1;
-let pos = 1;
-
-let gameLoop =
-setInterval(()=>{
- frame ++;
- if(frame>10) frame=1;
- zum.src =
- `walk/go_${frame}.png`;
- pos += 5;
- zombia.style.transform = `translate(${pos}px)`;
- if(pos>window.innerWidth) pos =-150;
-
- if (checkcollision() && ! 
-mush.classList.contains("jump")){
-    gameOver();
+function spawnxombia() {
+    zombies.push(createZombie());
 }
-},50)
+setInterval(()=> {
+    spawnxombia();
+    seed++;
+},3000);
 
 
+// ===========================
+// چک برخورد دقیق
+// ===========================
+function checkcollision(zombieEl) {
+    const mushRect = mushimg.getBoundingClientRect();
+    const zRect = zombieEl.getBoundingClientRect();
 
+    const offset = 15; // فاصله امن تا برخورد واقعی
+
+    return (
+        zRect.right - offset > mushRect.left + offset &&
+        zRect.left + offset < mushRect.right - offset &&
+        zRect.bottom - offset > mushRect.top + offset &&
+        zRect.top + offset < mushRect.bottom - offset
+    );
+}
+//پایان بازی
